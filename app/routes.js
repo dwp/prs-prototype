a85d1ccbad00b1f88aeb59e992f92497ed7af7d2
Paul Smith <mail@paulsmith.site>
2017-07-27 14:53:33 +0100

    adds initial postcode checking form

--- app/routes.js
+++ app/routes.js
@@ -6,6 +6,16 @@ router.get('/', function (req, res) {
   res.render('index')
 })
 
+// Route index page
+router.post('/postcode/redirect', function (req, res) {
+  if(req.body) {
+    console.log(req.body);
+  }
+  res.render('./postcode/request-form', {
+    postcode: (req.body.postcode ? req.body.postcode : 'not entered')
+  })
+})
+
 // add your routes here
 
 module.exports = router
