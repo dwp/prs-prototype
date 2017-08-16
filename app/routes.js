f5f2618ceb2fe94e74192904c1ef4a2f6c56764f
emollett <contenteditor@administrators-MacBook-Air-2.local>
2017-08-16 16:29:53 +0100

    secure/unsecure routing
    Asks if the user has gsi or gsx in their email, and sends them to the
    relevant form. Need to fix the postcode checker bit.

--- app/routes.js
+++ app/routes.js
@@ -8,11 +8,24 @@ router.get('/', function (req, res) {
 
 // Route index page
 router.post('/postcode/redirect', function (req, res) {
-  res.render('./postcode/request-form', {
+  res.render('./postcode_v3_1/full-service-email', {
     postcode: (req.body.postcode ? req.body.postcode : 'not entered')
   })
 })
 
+// Branching
+router.get('/postcode_v3_1/full-service-email', function (req, res) {
+  // get the answer from the query string (eg. ?over18=false)
+  var gsi = req.query.gsi
+
+  if (gsi === 'false') {
+    // redirect to the relevant page
+    res.redirect('/postcode_v3_1/request-form')
+  } else {
+    // if over18 is any other value (or is missing) render the page requested
+    res.redirect('/postcode_v3_1/request-form-secure')
+  }
+})
 // add your routes here
 
 module.exports = router
