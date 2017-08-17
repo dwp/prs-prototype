507c49c24e0a2183ed049d19e537b0c25bddb2be
emollett <contenteditor@administrators-MacBook-Air-2.local>
2017-08-17 17:30:57 +0100

    Outcome page down to one page
    postcode redirect works, and gsi/gsx checker now sends to one outcome
    page.

--- app/routes.js
+++ app/routes.js
@@ -22,26 +22,19 @@ router.post('/postcode_v3/redirect', function (req, res) {
 
 // Route index page v3.1
 router.post('/postcode_v3_1/redirect', function (req, res) {
-  res.render('./postcode_v3_1/full-service-email', {
+  res.render('./postcode_v3_1/email-type', {
     postcode: (req.body.postcode ? req.body.postcode : 'not entered')
   })
 })
 
 
 // Branching v3.1
-router.get('/postcode_v3_1/full-service-email', function (req, res) {
-  // get the answer from the query string (eg. ?over18=false)
-  var gsi = req.query.gsi
-
-  if (gsi === 'false') {
-    // redirect to the relevant page
-    res.redirect('/postcode_v3_1/request-form')
-  } else {
-    // if over18 is any other value (or is missing) render the page requested
-    res.redirect('/postcode_v3_1/request-form-secure')
-  }
+router.get('/postcode_v3_1/email-type', function (req, res) {
+  res.render('./postcode_v3_1/request-form')
 })
 
 // add your routes here
+
+
 
 module.exports = router
