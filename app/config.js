9fbfd4902afb739873f485ce1fda7f4ca104cb94
Ash-Wilson <ashleighwilson.digitaldwp@gmail.com>
2017-10-04 14:16:29 +0100

    Amends based on UR

--- app/config.js
+++ app/config.js
@@ -4,7 +4,7 @@
 
 module.exports = {
   // Service name used in header. Eg: 'Renew your passport'
-  serviceName: 'Direct rent payments for landlords',
+  serviceName: 'Apply for a direct payment of rent',
 
   // Default port that prototype runs on
   port: '3000',
