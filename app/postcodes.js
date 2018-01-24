76570ae8830bb5af10daacd2c5db4515b1aa99c5
Ash-Wilson <ashleighwilson.digitaldwp@gmail.com>
2018-01-24 12:50:58 +0000

    Working on add another and tweaks to make features work

--- app/postcodes.js
+++ app/postcodes.js
@@ -1,12 +1,12 @@
 const router = require('express').Router()
 
 const addresses = [
-  { address: [ '34 Carr Street', 'Ecclesall', 'Sheffield', 'S119PJ' ] },
-  { address: [ 'Flat 26', 'Nightingale Road', 'Newton Heath', 'M100AE' ] }
+    { address: ['34 Carr Street', 'Ecclesall', 'Sheffield', 'S119PJ'] },
+    { address: ['Flat 26', 'Nightingale Road', 'Newton Heath', 'M100AE'] }
 ]
 
 router.route('/:postcode').get((req, res) => {
-  res.json(addresses[req.params.postcode.toUpperCase() === 'S119PJ' ? 0 : 1])
+    res.json(addresses[req.params.postcode.toUpperCase() === 'S119PJ' ? 0 : 1])
 })
 
 module.exports = router
