0039089d0889fbcd4b35ae7af3649abb60e87d6f
Colin Rotherham <work@colinr.com>
2019-03-08 16:22:46 +0000

    Add initial live prototype

--- app/routes.js
+++ app/routes.js
@@ -1,6 +1,38 @@
 const express = require('express')
+const fs = require('fs')
 const router = express.Router()
 
 // Add your routes here - above the module.exports line
+
+// Data sources
+router.all('/data/:data/source/:source', (req, res) => {
+  const { data, source } = req.params
+  res.json(require(`./data/${data}/source/${source}`))
+})
+
+// Middleware
+router.use(require('./middleware/locals'))
+
+// Remove trailing slashes
+router.all('\\S+/$', (req, res) => {
+  res.redirect(301, req.path.slice(0, -1) + req.url.slice(req.path.length))
+})
+
+// Find prototypes here
+const search = `${__dirname}/views/prototypes/`
+const prototypes = fs.readdirSync(search).filter(file => {
+  return fs.statSync(`${search}/${file}`).isDirectory()
+})
+
+// Multiple prototypes
+for (const directory of prototypes) {
+  const prototype = require(`${search}${directory}`)
+
+  // Prototype static assets
+  prototype.use(`/assets`, express.static(`${__dirname}/views/prototypes/${directory}/assets`))
+
+  // Prototype router
+  router.use(`/${directory}`, prototype)
+}
 
 module.exports = router
