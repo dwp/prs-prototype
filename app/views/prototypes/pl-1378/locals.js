307293e6a740a4e75abc5256a70801fe50a6f605
Ashleigh Wilson <ashleighwilson.digitaldwp@gmail.com>
2019-09-19 14:51:33 +0100

    Linking from feedback banner and complete page to feedback survey

--- app/views/prototypes/pl-1378/locals.js
+++ app/views/prototypes/pl-1378/locals.js
@@ -1,5 +1,5 @@
 // Use this file to override locals
 
 module.exports = {
-  cookieText: 'This is a new service – your <a href="#">feedback</a> will help us to improve it.',
+  cookieText: 'This is a new service – your <a href="/pl-1378/questions/feedback-satisfied">feedback</a> will help us to improve it.',
 }
