52ef081d130d6219f27a03ec4e3c17151bb9217e
Ashleigh Wilson <ashleighwilson.digitaldwp@gmail.com>
2019-10-01 15:51:21 +0100

    Adding submit fail option v1

---
+++ app/views/prototypes/pl-1392/locals.js
@@ -0,0 +1,5 @@
+// Use this file to override locals
+
+module.exports = {
+  cookieText: 'This is a new service – your <a href="/pl-1378/questions/feedback-satisfied">feedback</a> will help us to improve it.',
+}
